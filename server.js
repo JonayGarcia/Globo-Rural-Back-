b440bdd1521859a2d3b9411bc574634471b520be
Eduardo Ortega Naranjo <eduardo.ortega1002@gmail.com>
2020-11-12 22:56:00 +0000

    Node connected to Mongo.

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
-mongoose.connect("mongodb://mongo_database/globo_rural");
+mongoose.connect("mongodb://mongo_database/globo_rural", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+app.get("/", function (request, response) {
+    return response.send("Hello World!");
+  });
 
 app.listen(8080);
